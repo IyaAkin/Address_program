aebc0909a04e5d99d61898aae2fb16dfa4faf330
IyaAkin <123050065+IyaAkin@users.noreply.github.com>
2023-01-19 15:25:36 +1030

    Add files via upload

---
+++ jaja.py
@@ -0,0 +1,16 @@
+
+# Create a program that displays your name and complete mailing address formatted in
+# the manner that you would usually see it on the outside of an envelope. Your program
+# does not need to read any input from the user.
+
+
+Name="Halimah Omoloja"
+Mailing_Address="7/19-27 Saint kitts place"
+Surburb="Mawson Lakes"
+State="SA"
+postcode=5095
+print(Name)
+print(Mailing_Address)
+print(Surburb)
+print(State)
+print(postcode)
